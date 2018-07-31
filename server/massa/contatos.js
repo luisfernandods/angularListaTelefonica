32375aa19ae9f879a3dc82e0e916197a90fe9516
Luis Fernando <luis_fernandods@hotmail.com>
2018-07-30 21:46:01 -0300

    Change massa

--- server/massa/contatos.js
+++ server/massa/contatos.js
@@ -1,46 +1,47 @@
-var massaCores = require('./cores'),
-    massaOperadoras = require('./operadoras');
+var massaCores = require("./cores"),
+  massaOperadoras = require("./operadoras");
 
 module.exports = {
-    contatos: [{
-        nome: "Luis",
-        telefone: "9999-9999",
-        operadora: {
-            nome: massaOperadoras.operadoras[0].nome
-        },
-        cor: {
-            valor: massaCores.cores[0].valor
-        }
+  contatos: [
+    {
+      nome: "Luis",
+      telefone: "9999-9999",
+      operadora: {
+        nome: massaOperadoras.operadoras[0].nome
+      },
+      cor: {
+        valor: massaCores.cores[0].valor
+      }
     },
     {
-        nome: "Joao",
-        telefone: "9999-8888",
-        operadora: {
-            nome: massaOperadoras.operadoras[2].nome
-        },
-        cor: {
-            valor: massaCores.cores[1].valor
-        }
+      nome: "Antonio",
+      telefone: "9999-8888",
+      operadora: {
+        nome: massaOperadoras.operadoras[2].nome
+      },
+      cor: {
+        valor: massaCores.cores[1].valor
+      }
     },
     {
-        nome: "Paulo",
-        telefone: "9999-7777",
-        operadora: {
-            nome: massaOperadoras.operadoras[1].nome
-        },
-        cor: {
-            valor: massaCores.cores[2].valor
-        }
+      nome: "Paulo",
+      telefone: "9999-7777",
+      operadora: {
+        nome: massaOperadoras.operadoras[1].nome
+      },
+      cor: {
+        valor: massaCores.cores[2].valor
+      }
     },
     {
-        nome: "Maria",
-        telefone: "9999-6666",
-        operadora: {
-            nome: massaOperadoras.operadoras[3].nome
-        },
-        cor: {
-            valor: massaCores.cores[3].valor
-        }
-    },
-    ]
-}
+      nome: "Maria",
+      telefone: "9999-6666",
+      operadora: {
+        nome: massaOperadoras.operadoras[3].nome
+      },
+      cor: {
+        valor: massaCores.cores[3].valor
+      }
+    }
+  ]
+};
